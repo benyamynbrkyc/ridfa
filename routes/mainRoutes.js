d205ee12c408f67acf8b9dc8e7ae0b31c92ffd56
benyamynbrkyc <benjamin.brkic@gmail.com>
2020-07-08 02:18:19 +0200

    pre prod

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -6,4 +6,16 @@ mainRoutes.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+mainRoutes.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '../about.html'))
+})
+
+mainRoutes.get('/portfolio', (req, res) => {
+  res.sendFile(path.join(__dirname, '../portfolio.html'))
+})
+
+mainRoutes.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '../contact.html'))
+})
+
 module.exports = mainRoutes
